ecf81f482ffbb5844e995a668a8e34812fb9f944
ilyasm0919 <ilyasm0919@gmail.com>
2022-12-13 18:50:51 +0300

    Better parse errors

--- GeometryAndroid/app/src/main/java/com/ibis/geometry/common/Parse.kt
+++ GeometryAndroid/app/src/main/java/com/ibis/geometry/common/Parse.kt
@@ -20,7 +20,7 @@ fun ParseContext.parseLine(init: String): Reactive<Drawable> {
     val (modifiers, line) = parseModifiers(init)
     val index = line.indexOf("=")
     val value = line.substring(index + 1).trimStart()
-    val (res, next) = parseSum(value) ?: error("Parsing failed: $value")
+    val (res, next) = parseSum(value)
     check(next.isBlank()) { "Line not consumed: $next" }
     val name = index.takeIf { it != -1 }?.let {
         line.substring(0, it).trimEnd()
@@ -36,14 +36,14 @@ fun ParseContext.parseLine(init: String): Reactive<Drawable> {
 
 fun<T> parseInfix(
     init: String,
-    parser: (String) -> Pair<T, String>?,
+    parser: (String) -> Pair<T, String>,
     vararg ops: Pair<String, (T, T) -> T>
-): Pair<T, String>? {
-    var (res, line) = parser(init) ?: return null
+): Pair<T, String> {
+    var (res, line) = parser(init)
     line = line.trimStart()
     while (true) {
         val op = ops.firstOrNull { line.startsWith(it.first) } ?: return res to line
-        val (next, nextLine) = parser(line.substring(op.first.length).trimStart()) ?: return res to line
+        val (next, nextLine) = parser(line.substring(op.first.length).trimStart())
         res = op.second(res, next)
         line = nextLine
     }
@@ -55,17 +55,17 @@ fun ParseContext.parseSum(line: String) =
 fun ParseContext.parseProd(line: String) =
     parseInfix(line, ::parseApp, "*" to Complex::times::precast, "/" to Complex::div::precast)
 
-fun ParseContext.parseApp(line: String): Pair<Reactive<Geometric>, String>? = when {
+fun ParseContext.parseApp(line: String): Pair<Reactive<Geometric>, String> = when {
     line.startsWith("~") ->
-        parseApp(line.substring(1).trimStart())?.mapFst(Complex::conj::precast)
+        parseApp(line.substring(1).trimStart()).mapFst(Complex::conj::precast)
     line.startsWith("(") ->
-        parseSum(line.substring(1).trimStart())?.mapSnd {
-            check(it.startsWith(")")) { "Expected ')'" }
+        parseSum(line.substring(1).trimStart()).mapSnd {
+            check(it.startsWith(")")) { IllegalArgumentException("Expected ')'") }
             it.substring(1)
         }
     line.startsWith("#") ->
-        parseApp(line.substring(1).trimStart())?.let {
-            check(it.first is Static && (it.first as Static<*>).value is Complex) { "Expected point" }
+        parseApp(line.substring(1).trimStart()).let {
+            check(it.first is Static && (it.first as Static<*>).value is Complex) { "Expected static point" }
             movable.add(Movable((it.first as Static<*>).value as Complex,
                 line.substring(0, line.length - it.second.length)))
             it
@@ -73,19 +73,22 @@ fun ParseContext.parseApp(line: String): Pair<Reactive<Geometric>, String>? = wh
     else -> parseAtom(line)
 }
 
-fun ParseContext.parseAtom(line: String): Pair<Reactive<Geometric>, String>? =
-    parseWord(line).let { (word, next) ->
+fun ParseContext.parseAtom(line: String): Pair<Reactive<Geometric>, String> =
+    parseWord(line)?.let { (word, next) ->
         if (next.startsWith("(")) {
-            val (args, next2) = parseInfix(next.substring(1), { parseSum(it)?.mapFst(::listOf) },
-                "," to { a, b -> a + b })
-                ?: (emptyList<Reactive<Geometric>>() to next.substring(1).trimStart())
+            val (args, next2) = if (next.substring(1).trimStart().startsWith(")"))
+                emptyList<Reactive<Geometric>>() to next.substring(1).trimStart()
+            else parseInfix(next.substring(1),
+                { parseSum(it).mapFst(::listOf) }, "," to { a, b -> a + b }
+            )
             check(next2.startsWith(")")) { "Expected ')'" }
-            function(word, args)?.to(next2.substring(1).trimStart())
+            function(word, args) to next2.substring(1).trimStart()
         }
-        else names[word]?.to(next)
+        else names[word]?.to(next) ?: error("Name not found: $word")
     } ?: parseComplex(line)?.let { (res, next) -> Static(res) to next.trimStart() }
+    ?: error("Parsing failed: $line")
 
-fun function(name: String, args: Collection<Reactive<Geometric>>): Reactive<Geometric>? {
+fun function(name: String, args: Collection<Reactive<Geometric>>): Reactive<Geometric> {
     fun reactive(action: ArgParser.() -> Geometric) = args.traverse {
         parseArgs(it, action)
     }
@@ -142,16 +145,17 @@ fun function(name: String, args: Collection<Reactive<Geometric>>): Reactive<Geom
         }
         "line_trace" -> Static(args.single().let {
             line(
-                it(ReactiveInput(3658))  as? Complex ?: error("Expected point"),
-                it(ReactiveInput(9137))  as? Complex ?: error("Expected point")
+                it(ReactiveInput(3658)) as? Complex ?: error("Expected point"),
+                it(ReactiveInput(9137)) as? Complex ?: error("Expected point")
             )
         })
         "circle_trace" -> Static(args.single().let {
             circumcircle(
                 Triangle(
-                it(ReactiveInput(2709))  as? Complex ?: error("Expected point"),
-                it(ReactiveInput(5073))  as? Complex ?: error("Expected point"),
-                it(ReactiveInput(6349))  as? Complex ?: error("Expected point"))
+                    it(ReactiveInput(2709)) as? Complex ?: error("Expected point"),
+                    it(ReactiveInput(5073)) as? Complex ?: error("Expected point"),
+                    it(ReactiveInput(6349)) as? Complex ?: error("Expected point")
+                )
             )
         })
         "assert" -> reactive {
@@ -160,7 +164,7 @@ fun function(name: String, args: Collection<Reactive<Geometric>>): Reactive<Geom
             check((x - y).norm < x.norm * 0.0001f) { "Assertion failed: $x != $y" }
             x
         }
-        else -> null
+        else -> error("Fun not found: $name")
     }
 }
 
@@ -180,7 +184,7 @@ fun parseComplex(line: String) = when {
     }
 }
 
-fun parseModifier(line: String): Pair<(Style) -> Unit, String> = parseWord(line).let { (word, next) ->
+fun parseModifier(line: String): Pair<(Style) -> Unit, String> = parseWord(line)?.let { (word, next) ->
     fun set(action: Style.() -> Unit): (Style) -> Unit = action
     when (word) {
         "hide" -> set { border = Border.No }
@@ -198,7 +202,7 @@ fun parseModifier(line: String): Pair<(Style) -> Unit, String> = parseWord(line)
         "black" -> set { color = Color(0xFF000000) }
         else -> error("Invalid modifier: $word")
     } to next
-}
+} ?: error("Expected modifier: $line")
 
 fun parseModifiers(init: String): Pair<List<(Style) -> Unit>, String> {
     var line = init.trimStart()
@@ -212,7 +216,7 @@ fun parseModifiers(init: String): Pair<List<(Style) -> Unit>, String> {
     return modifiers to line
 }
 
-fun parseWord(line: String): Pair<String, String> =
-    line.takeWhile { it.isLetterOrDigit() || it in "_'{}" }.let {
+fun parseWord(line: String): Pair<String, String>? =
+    line.takeWhile { it.isLetterOrDigit() || it in "_'{}" }.takeIf { it.isNotEmpty() && it[0].isLetter() }?.let {
         it to line.substring(it.length).trimStart()
     }
