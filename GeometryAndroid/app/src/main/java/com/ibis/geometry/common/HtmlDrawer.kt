ea7c3cf23cb157a6d5c65475acd21634233aea97
ilyasm0919 <ilyasm0919@gmail.com>
2022-12-24 17:53:40 +0300

    Html optimized (blank chars removed)

--- GeometryAndroid/app/src/main/java/com/ibis/geometry/common/HtmlDrawer.kt
+++ GeometryAndroid/app/src/main/java/com/ibis/geometry/common/HtmlDrawer.kt
@@ -10,106 +10,77 @@ class HtmlDrawer(size: Size, val writer: OutputStreamWriter): Drawer {
     override val size = size * 200f / size.minDimension
 
     init {
-        writer.write("""
-            <!DOCTYPE html>
-            <html>
-            <body>
-            <canvas id="canvas" width="${size.width}" height="${size.height}"></canvas>
-            <script>
-            const ctx = document.getElementById("canvas").getContext("2d");
-            ctx.translate(${size.center.x}, ${size.center.y});
-            ctx.scale(${size.minDimension / 200}, ${size.minDimension / 200});
-            ctx.lineWidth = 0.8;
-            
-        """.trimIndent())
+        writer.write("<!DOCTYPE html>" +
+                "<html>" +
+                "<body>" +
+                "<canvas id=\"canvas\" width=\"${size.width}\" height=\"${size.height}\"></canvas>" +
+                "<script>" +
+                "const ctx=document.getElementById(\"canvas\").getContext(\"2d\");" +
+                "ctx.translate(${size.center.x},${size.center.y});" +
+                "ctx.scale(${size.minDimension / 200},${size.minDimension / 200});" +
+                "ctx.lineWidth=0.8;")
     }
 
     fun finish() {
-        writer.write("""
-            </script>
-            </body>
-            </html>
-            
-        """.trimIndent())
+        writer.write("</script></body></html>")
         writer.close()
     }
 
     override fun point(offset: Offset, color: Color) {
-        writer.write("""
-            ctx.beginPath();
-            ctx.arc(${offset.x}, ${offset.y}, 1.8, 0, 2*Math.PI);
-            ctx.fillStyle = "${color.svg}";
-            ctx.fill();
-            
-        """.trimIndent())
+        writer.write("ctx.beginPath();" +
+                "ctx.arc(${offset.x},${offset.y},1.8,0,2*Math.PI);" +
+                "ctx.fillStyle=\"${color.svg}\";" +
+                "ctx.fill();")
     }
 
     override fun circle(center: Offset, radius: Float, style: Style) = style.styled {
-        writer.write("ctx.arc(${center.x}, ${center.y}, $radius, 0, 2*Math.PI);\n")
+        writer.write("ctx.arc(${center.x},${center.y},$radius,0,2*Math.PI);")
     }
 
     override fun line(from: Offset, to: Offset, style: Style) = style.styled {
-        writer.write("""
-            ctx.moveTo(${from.x}, ${from.y});
-            ctx.lineTo(${to.x}, ${to.y});
-            
-        """.trimIndent())
+        writer.write("ctx.moveTo(${from.x},${from.y});" +
+                "ctx.lineTo(${to.x},${to.y});")
     }
 
     override fun polygon(points: List<Offset>, style: Style) = style.styled {
-        writer.write("ctx.moveTo(${points[0].x}, ${points[0].y});\n")
+        writer.write("ctx.moveTo(${points[0].x},${points[0].y});")
         points.drop(1).forEach {
-            writer.write("ctx.lineTo(${it.x}, ${it.y});\n")
+            writer.write("ctx.lineTo(${it.x},${it.y});")
         }
-        writer.write("ctx.closePath();\n")
+        writer.write("ctx.closePath();")
     }
 
     override fun text(pos: Offset, text: List<String>, color: Color) {
-        writer.write("""
-            ctx.beginPath();
-            var x = ${pos.x + 3}
-            
-        """.trimIndent())
+        writer.write("ctx.beginPath();" +
+                "var x=${pos.x + 3};")
         text.forEachIndexed { index, str ->
-            writer.write("""
-                ctx.font = "${if (index % 2 == 0) 8 else 6}px roboto";
-                ctx.fillText("$str", x, ${pos.y + if (index % 2 == 0) 3 else 6});
-                x += ctx.measureText("$str").width;
-                
-            """.trimIndent())
+            writer.write("ctx.font=\"${if (index % 2 == 0) 8 else 6}px roboto\";" +
+                    "ctx.fillText(\"$str\",x,${pos.y + if (index % 2 == 0) 3 else 6});" +
+                    "x+=ctx.measureText(\"$str\").width;")
         }
     }
 
     override fun angle(center: Offset, from: Float, to: Float, style: Style) = style.styled {
-        writer.write("""
-            ctx.arc(${center.x}, ${center.y}, 10, ${-from}, ${-to}, ${from < to});
-            ctx.lineTo(${center.x}, ${center.y});
-            ctx.closePath();
-            
-        """.trimIndent())
+        writer.write("ctx.arc(${center.x},${center.y},10,${-from},${-to},${from < to});" +
+                "ctx.lineTo(${center.x},${center.y});" +
+                "ctx.closePath();")
     }
 
     private fun Style.styled(action: () -> Unit) = if (fill || border != Border.No) {
-        writer.write("ctx.beginPath();\n")
+        writer.write("ctx.beginPath();")
         action()
         if (fill) {
-            writer.write("""
-                ctx.fillStyle = ${color.html(0.3f)};
-                ctx.fill();
-                
-            """.trimIndent())
+            writer.write("ctx.fillStyle=${color.html(0.3f)};" +
+                    "ctx.fill();")
         }
         if (border != Border.No) {
             border.effect?.toList().orEmpty().let {
-                writer.write("ctx.setLineDash([${it.joinToString()}]);\n")
+                writer.write("ctx.setLineDash([${it.joinToString()}]);")
             }
-            writer.write("""
-                ctx.strokeStyle = "${color.svg}";
-                ctx.stroke();
-                
-            """.trimIndent())
+            writer.write("ctx.strokeStyle=\"${color.svg}\";" +
+                    "ctx.stroke();")
         } else Unit
     } else Unit
 }
 
-fun Color.html(alpha: Float) = "\"rgba(${red*255}, ${green*255}, ${blue*255}, $alpha)\""
+fun Color.html(alpha: Float) = "\"rgba(${red*255},${green*255},${blue*255},$alpha)\""
--- GeometryAndroid/app/src/main/java/com/ibis/geometry/common/HtmlVideo.kt
+++ GeometryAndroid/app/src/main/java/com/ibis/geometry/common/HtmlVideo.kt
@@ -4,32 +4,24 @@ import androidx.compose.ui.geometry.center
 
 data class HtmlVideo(val drawer: HtmlDrawer, val rate: Number, var frame: Int): Screenshot.Record() {
     init {
-        drawer.writer.write("""
-            var play = true;
-            document.onkeyup = function(e) { if (e.keyCode == 32) play = !play; };
-            var time = 0;
-            setInterval(function() {
-            if (!play) return;
-            ctx.clearRect(${-drawer.size.center.x}, ${-drawer.size.center.y}, ${drawer.size.width}, ${drawer.size.height});
-            switch(time) {
-            
-        """.trimIndent())
+        drawer.writer.write("var play=true;" +
+                "document.onkeyup=function(e){if(e.keyCode==32)play=!play;};" +
+                "var time=0;" +
+                "setInterval(function(){" +
+                "if(!play)return;" +
+                "ctx.clearRect(${-drawer.size.center.x},${-drawer.size.center.y},${drawer.size.width},${drawer.size.height});" +
+                "switch(time){")
     }
 
     override fun frame(content: Drawer.() -> Unit) {
-        drawer.writer.write("case $frame:\n")
+        drawer.writer.write("case $frame:")
         drawer.content()
-        drawer.writer.write("break;\n")
+        drawer.writer.write("break;")
         frame++
     }
 
     override fun finish() {
-        drawer.writer.write("""
-            }
-            time = (time + 1) % $frame;
-            }, $rate);
-            
-        """.trimIndent())
+        drawer.writer.write("}time = (time+1)%$frame;},$rate);")
         drawer.finish()
     }
 }
