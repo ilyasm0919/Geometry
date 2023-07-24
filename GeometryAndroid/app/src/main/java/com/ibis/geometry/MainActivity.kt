66abb03303bdd2eb9f7198ba300612f46e4fd00f
ilyasm0919 <ilyasm0919@gmail.com>
2023-07-24 23:54:14 +0300

    Android version fixed

--- GeometryAndroid/app/src/main/java/com/ibis/geometry/MainActivity.kt
+++ GeometryAndroid/app/src/main/java/com/ibis/geometry/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.text.ExperimentalTextApi
 import androidx.core.net.toUri
+import androidx.documentfile.provider.DocumentFile
 import com.ibis.geometry.common.App
 import com.ibis.geometry.common.theme.GeometryTheme
 import java.io.File
@@ -17,7 +18,18 @@ class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalTextApi::class, ExperimentalMaterialApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val inputFile = mutableStateOf(intent.data ?: File(getExternalFilesDir(null), "input.geo").toUri())
+        val inputFile = mutableStateOf(intent.data?.let { uri ->
+            if (DocumentFile.fromSingleUri(this, uri)?.canWrite() == true) uri
+            else File(getExternalFilesDir(null), "input.geo").also {
+                it.outputStream().use { o ->
+                    contentResolver.openInputStream(uri)?.use { i ->
+                        i.copyTo(o)
+                    }
+                }
+            }.toUri()
+        } ?: File(getExternalFilesDir(null), "input.geo").also {
+            it.createNewFile()
+        }.toUri())
         val globalFile = File(getExternalFilesDir(null), "global.geo")
         globalFile.createNewFile()
 
